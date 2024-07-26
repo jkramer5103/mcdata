4d1abdc3640b8b90209baa6a6fb83b628c602a24
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-07-26 17:29:18 +0200

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -13,10 +13,10 @@ mkdir mc
 cd mc
 
 # Download Spigot server JAR file
-wget https://download.getbukkit.org/spigot/spigot-1.20.4.jar
+wget https://download.getbukkit.org/spigot/spigot-1.21.jar
 
 # Create start script for Spigot server
-echo "screen -dmS mcServer java -Xmx1024M -Xms1024M -jar spigot-1.20.4.jar" > start.sh
+echo "screen -dmS mcServer java -Xmx1024M -Xms1024M -jar spigot-1.21.jar" > start.sh
 chmod +x start.sh
 
 # Agree to Minecraft EULA
